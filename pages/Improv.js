9c3488444c8f75a2c5e92b74d9c1561f395bf072
StephenLeshko <stephen.leshko@gmail.com>
2023-02-21 19:59:31 -0500

    api

--- pages/Improv.js
+++ pages/Improv.js
@@ -6,7 +6,25 @@ import { Inter } from '@next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Improv() {
+  
+  
+  async function handleClick() {
+    console.log('click handling')
+    const response = await fetch('/api/generate', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      // body: JSON.stringify({ingredients: inputValue})
+    })
+    const data = await response.json()
+    console.log(data.recipe)
+  }
+  
   return (
+      <div>
         <h1>Improv</h1>
+        <button onClick={handleClick}>Button</button>
+      </div>
   )
 }
